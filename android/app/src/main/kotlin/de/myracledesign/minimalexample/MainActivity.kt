219686b165ac6948168e912e4f1e6d57dee5bf66
myracledesign <weber.max.1988@live.de>
2020-06-14 14:17:49 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/de/myracledesign/minimalexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.myracledesign.minimalexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChangeNotifierProvider(
+        create: (context) => AppStateProvider(),
+        child: Dashboard(),
+      ),
+    );
+  }
+}
+
+class StateChanger extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          RaisedButton(
+            child: Text("Press me to change Dashboard"),
+            onPressed: () {
+              // Change state on page 2
+              Provider.of<AppStateProvider>(context, listen: false).changeValue(
+                Random().nextInt(500),
+              );
+            },
+          ),
+          RaisedButton(
+            child: Text("Navigate back"),
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(
+              "Here is your value, ${Provider.of<AppStateProvider>(context).value}"),
+          RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => StateChanger(),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AppStateProvider extends ChangeNotifier {
+  int _value;
+
+  void changeValue(int newValue) {
+    _value = newValue;
+    notifyListeners();
+  }
+
+  get value => _value;
+}
